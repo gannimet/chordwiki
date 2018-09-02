3e0c50b82ba7e4f9cfd67ac100353f2fbe20f4b7
Richard Wotzlaw <r.wotzlaw@gmail.com>
2018-09-02 19:56:46 +0200

    added react-router and setup basic routing structure

---
+++ src/new_song/NewSong.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './NewSong.css';
+
+class NewSong extends Component {
+    
+    render() {
+        return (
+            <div>I am the NewSong component</div>
+        );
+    }
+    
+}
+
+export default NewSong;
---
+++ src/song/Song.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './Song.css';
+
+class Song extends Component {
+    
+    render() {
+        return (
+            <div>I am the Song component</div>
+        );
+    }
+    
+}
+
+export default Song;
